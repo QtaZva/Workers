50bf65ec187780735ff1f4d5466eb352d2615ae6
QtaZva <qtazva@mail.ru>
2024-04-23 11:20:24 +0900

    Славянский зажим FIX-ом

--- View/WindowEmployee.xaml.cs
+++ View/WindowEmployee.xaml.cs
@@ -25,10 +25,26 @@ namespace Workers.View
     /// </summary>
     public partial class WindowEmployee : Window
     {
+
+        private PersonViewModel vmPerson = new PersonViewModel();
+        private RoleViewModel vmRole;
+        private ObservableCollection<PersonDpo> personsDPO;
+        private List<Role> roles;
         public WindowEmployee()
         {
             InitializeComponent();
             DataContext = new PersonViewModel();
+            vmPerson = new PersonViewModel();
+            vmRole = new RoleViewModel();
+            roles = vmRole.ListRole.ToList();
+            personsDPO = new ObservableCollection<PersonDpo>();
+            foreach (var person in vmPerson.ListPerson)
+            {
+                PersonDpo p = new PersonDpo();
+                p = p.CopyFromPerson(person);
+                personsDPO.Add(p);
+            }
+            lvEmployee.ItemsSource = personsDPO;
         }
     }
 }
--- View/WindowNewEmployee.xaml.cs
+++ View/WindowNewEmployee.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Workers.ViewModel;
 
 namespace Workers.View
 {
@@ -22,6 +23,7 @@ namespace Workers.View
         public WindowNewEmployee()
         {
             InitializeComponent();
+            CbRole.ItemsSource = new RoleViewModel().ListRole;
         }
 
         private void BtSave_Click(object sender, RoutedEventArgs e)
